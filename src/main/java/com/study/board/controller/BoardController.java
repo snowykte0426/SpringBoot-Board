f6c282fe9a4369a9f8aea87d3e4d7a64b0bc676c
snowykte0426 <snowykte0426@naver.com>
2024-04-12 22:43:20 +0900

    게시글 작성,리스트 구현

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -4,20 +4,16 @@ import com.study.board.entity.Board;
 import com.study.board.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-
-import javax.sound.midi.Soundbank;
 
 
 @Controller
 public class BoardController {
     @Autowired
-    private final BoardService boardService;
+    private BoardService boardService;
 
-    public BoardController(BoardService boardService) {
-        this.boardService = boardService;
-    }
 
     @GetMapping("/board/write")//localhost:8080/board/write
     public String boardwriteFrom() {
@@ -31,5 +27,12 @@ public class BoardController {
         return "";
 
     }
+
+    @GetMapping("/board/list")
+    public String boardList(Model model) {
+        model.addAttribute("list",boardService.boardList());
+        return "boardlist";
+    }
+
 }
 
